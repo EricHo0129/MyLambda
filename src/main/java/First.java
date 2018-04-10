0b65a6fe3964f3828dc7c0d0e4016742345a671c
EricHo0129 <yung.ho@104.com.tw>
2018-04-10 16:51:34 +0800

    First init

---
+++ src/main/java/First.java
@@ -0,0 +1,12 @@
+
+public class First {
+
+	/**
+	 * 測試Lambda環境
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Runnable r = () -> System.out.println("我的第一個Lambda測試");
+		r.run();
+	}
+}
